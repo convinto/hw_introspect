a59b34a203d704c5bd687f7884db4eb9e6e4b558
convinto <convinto@yandex.ru>
2024-10-27 19:26:09 +0300

    Add files via upload

--- hw_introspect.py
+++ hw_introspect.py
@@ -1,14 +1,18 @@
 def introspection_info(obj):
-    type_obj = type(obj).__name__                                                   #Тип объекта.
-    atr_obj = dir(obj)                                                              #Атрибуты объекта.
-    method_obj = [method for method in atr_obj if callable(getattr(obj, method))]   #Методы объекта.
-    module_obj = obj.__class__.__module__                                   #Модуль, к которому объект принадлежит.
+    type_obj = type(obj).__name__                                       # Тип объекта
+    attributes = list(obj.__class__.__dict__.keys())                    # Атрибуты объекта
+    methods = [method for method in dir(obj) if callable(getattr(obj, method))]  # Методы объекта
+    module_obj = obj.__class__.__module__                               # Модуль, к которому объект принадлежит
 
-    result = (f'type:{type_obj}\n'
-              f'attributes:{atr_obj}\n'
-              f'methods:{method_obj}\n'
-              f'module:{module_obj}')
+    result = (
+        f"Тип объекта: {type_obj}\n"
+        f"Атрибуты объекта: {', '.join(attributes)}\n"
+        f"Методы объекта: {', '.join(methods)}\n"
+        f"Модуль: {module_obj}"
+    )
+
     return result
+
 
 number_info = introspection_info(42)
 print(number_info)
